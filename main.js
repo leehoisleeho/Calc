982c73537a51cb5557daef80f53429068b25fe0c
leeho <leehovip@gmail.com>
2020-11-04 20:41:15 +0800

    初始化页面

---
+++ main.js
@@ -0,0 +1,9 @@
+// let width = document.body.offsetWidth
+// let height = window.screen.height
+// console.log(width)
+// console.log(height)
+// document.getElementById("monitor").style.height=height*0.2+"px"
+// document.getElementById("keyboard").style.height=height*0.8+"px"
+
+console.log("123")
+let width_monitor = document.getElementById("monitor").offsetHeight
